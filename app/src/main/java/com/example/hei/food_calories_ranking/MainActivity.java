4f7c81943fec06d1f266e166086aa19ed8ff4929
pokehei2 <pokehei2@yahoo.com.hk>
2017-09-25 23:43:48 +0900

    added マクドナルド, change api url. -A0.11

--- app/src/main/java/com/example/hei/food_calories_ranking/MainActivity.java
+++ app/src/main/java/com/example/hei/food_calories_ranking/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.hei.food_calories_ranking;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationProvider;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -46,14 +49,16 @@ public class MainActivity extends AppCompatActivity {
     private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
     private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
-
-    Food i1 = new Food("牛丼-並盛", "すき家", 650, 350);
-    Food i2 = new Food("ねぎ玉牛丼-並盛", "すき家", 768, 470);
-    Food i3 = new Food("牛めし-並盛", "松屋", 709, 290);
-    Food i4 = new Food("牛焼肉定食", "松屋", 776, 590);
-    Food i5 = new Food("豚生姜焼き定食-並盛", "吉野家", 627, 490);
-    Food i6 = new Food("秋のベジ牛定食-並盛", "吉野家", 651, 590);
-
+    // Arraylist constructor for calories data.
+    Food sukiya_i1 = new Food("牛丼-並盛", "すき家", 650, 350);
+    Food sukiya_i2 = new Food("ねぎ玉牛丼-並盛", "すき家", 768, 470);
+    Food matsuya_i1 = new Food("牛めし-並盛", "松屋", 709, 290);
+    Food matsuya_i2 = new Food("牛焼肉定食", "松屋", 776, 590);
+    Food yoshinoya_i1 = new Food("豚生姜焼き定食-並盛", "吉野家", 627, 490);
+    Food yoshinoya_i2 = new Food("秋のベジ牛定食-並盛", "吉野家", 651, 590);
+    Food macdon_i1 = new Food("フィレオフィッシュ", "マクドナルド", 341, 320);
+    Food macdon_i2 = new Food("てりやきマックバーガー", "マクドナルド", 496, 320);
+    // Arraylist constructor for array adapter.
     ArrayList<Food> foods = new ArrayList<>();
 
     @Override
@@ -63,20 +68,30 @@ public class MainActivity extends AppCompatActivity {
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mFusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
-                        // New location has now been determined
-                        // Location Storage
-                        String latitude = Double.toString(location.getLatitude());
-                        String longitude = Double.toString(location.getLongitude());
+                            // New location has now been determined
+                            // Location Storage
+                            String latitude = Double.toString(location.getLatitude());
+                            String longitude = Double.toString(location.getLongitude());
 
-                        TextView currentLocationStorage = (TextView) findViewById(current_location_storage);
-                        currentLocationStorage.setText(latitude + "," + longitude);
-                        Log.v("Tag", "currentLocation =_ " + currentLocationStorage);
+                            TextView currentLocationStorage = (TextView) findViewById(current_location_storage);
+                            currentLocationStorage.setText(latitude + "," + longitude);
+                            Log.v("Tag", "currentLocation =_ " + currentLocationStorage);
                         }
                     }
                 });
@@ -86,30 +101,8 @@ public class MainActivity extends AppCompatActivity {
 
         SettingsClient client = LocationServices.getSettingsClient(this);
         Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-
-//        startLocationUpdates();
-
-//        // Construct the data source
-//        final ArrayList<Food> foods = new ArrayList<>();
-//        foods.add(new Food("鮭おにぎり", "Supermarket", 150, 110));
-//        foods.add(new Food("親子丼", "Supermarket", 800, 338));
-//        foods.add(new Food("牛丼-並盛", "すき家", 650, 350));
-//        foods.add(new Food("ねぎ玉牛丼-並盛", "すき家", 768, 470));
-//        foods.add(new Food("グラン クラブハウス", "Macdonald", 516, 490));
-
-//
-//
-//        // Create the adapter to convert the array to views
-//        FoodAdapter adapter = new FoodAdapter(this, foods);
-//        // Attach the adapter to a ListView
-//        ListView listView = (ListView) findViewById(R.id.food_list_view);
-//        listView.setAdapter(adapter);
-
         handler.postDelayed(runnable, 2 * 1000);
-
     }
-
 
     protected void createLocationRequest() {
         LocationRequest mLocationRequest = new LocationRequest();
@@ -134,17 +127,17 @@ public class MainActivity extends AppCompatActivity {
         //Constructor of current location storage.
         TextView getLatLong = (TextView) findViewById(current_location_storage);
 
-        // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.7890371,139.8959144&radius=500&type=restaurant&keyword=Gyudon%20Restaurant&key=AIzaSyAgE1lUCVlNpi7OyTG6sUzd-CKN-nPeanY
-
+//         https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.7890371,139.8959144&radius=500&type=restaurant&keyword=Gyudon%20Restaurant&key=AIzaSyAgE1lUCVlNpi7OyTG6sUzd-CKN-nPeanY
+//
 //        setApiUrl.setText("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
 //                + getLatLong.getText() + "&radius=1000&type=restaurant&keyword=Gyudon%20Restaurant&key=AIzaSyAgE1lUCVlNpi7OyTG6sUzd-CKN-nPeanY");
-        // get text in google_api_url.
+//         get text in google_api_url.
         mApiLoc = getLatLong.getText().toString();
         return mApiLoc;
     }
 
     public class GetMapData extends AsyncTask<String, Void, String> {
-
+        // Result storage variable.
         String apiResult = null;
 
         @Override
@@ -158,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             HttpHandler sh = new HttpHandler();
             // Making a request to url and getting response
             String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + mApiLoc
-                    +"&radius=1000&type=restaurant&keyword=Gyudon%20Restaurant&key=AIzaSyAgE1lUCVlNpi7OyTG6sUzd-CKN-nPeanY";
+                    +"&radius=600&type=restaurant&keyword=fastfood%20Restaurant&key=AIzaSyAgE1lUCVlNpi7OyTG6sUzd-CKN-nPeanY";
             String jsonStr = sh.makeServiceCall(url);
 
             Log.e(TAG, "Response from url: " + jsonStr);
@@ -167,12 +160,10 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject jsonObj = new JSONObject(jsonStr);
                     // Getting JSON Array node
                     JSONArray jsonResult = jsonObj.getJSONArray("results");
-
                     // looping through All Contacts
                     for (int i = 0; i < jsonResult.length(); i++) {
                         JSONObject currentData = jsonResult.getJSONObject(i);
                         String name = currentData.getString("name");
-                        String rating = currentData.getString("rating");
 
                         if (name.contains("すき家")){
                             apiResult = apiResult + "すき家";
@@ -184,11 +175,10 @@ public class MainActivity extends AppCompatActivity {
                             apiResult = apiResult + "吉野家";
                         }
 
-//                        FoodBrandClass restaurant = new FoodBrandClass(name, rating);
-//                        restaurantsList.add(restaurant);
-
+                        if (name.contains("マクドナルド")){
+                            apiResult = apiResult + "マクドナルド";
+                        }
                         Log.d("Tag", "forLoop =_ " + name);
-
                     }
                 } catch (final JSONException e) {
                     Log.e(TAG, "Json parsing error: " + e.getMessage());
@@ -201,7 +191,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                 }
-
             } else {
                 Log.e(TAG, "Couldn't get json from server.");
                 runOnUiThread(new Runnable() {
@@ -220,33 +209,25 @@ public class MainActivity extends AppCompatActivity {
             protected void onPostExecute (String apiResult){
 //               super.onPostExecute();
 
-                Log.d("Tag", "apiResult =_ " + apiResult);
-
-//                boolean checkSukiya = restaurantsList.toString().contains("すき家");
-//                boolean checkMatsuya = restaurantsList.contains("松屋");
-//                int arrayListSize = restaurantsList.size();
-
-//                Log.v("Tag", "result =_ contained " + checkSukiya);
-//                Log.d("Tag", "result =_ contained " + checkMatsuya);
-//                Log.d("Tag", "result =_ Size " + arrayListSize);
-
-//                for (int i = 0; i<arrayListSize; i++){
-//                    Log.v ("Tag", "for_loop =_ " + restaurantsList.get(i).getRating()) ;
-//                }
-
+                // if logic for display data in UI.
                 if (apiResult.contains("すき家")) {
-                    foods.add(i1);
-                    foods.add(i2);
+                    foods.add(sukiya_i1);
+                    foods.add(sukiya_i2);
                 }
 
                 if (apiResult.contains("松屋")) {
-                    foods.add(i3);
-                    foods.add(i4);
+                    foods.add(matsuya_i1);
+                    foods.add(matsuya_i2);
                 }
 
                 if (apiResult.contains("吉野家")) {
-                    foods.add(i5);
-                    foods.add(i6);
+                    foods.add(yoshinoya_i1);
+                    foods.add(yoshinoya_i2);
+                }
+
+                if (apiResult.contains("マクドナルド")) {
+                    foods.add(macdon_i1);
+                    foods.add(macdon_i2);
                 }
 
                 // Create the adapter to convert the array to views
