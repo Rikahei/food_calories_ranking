8e5aae824573faa7d1d07922c8815a40dca61767
pokehei2 <pokehei2@yahoo.com.hk>
2017-10-18 00:32:50 +0900

    upload View Holder.java  -A0.133

---
+++ app/src/main/java/com/example/hei/food_calories_ranking/ViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.hei.food_calories_ranking;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewHolder {
+    TextView foodName;
+    TextView foodBrand;
+    TextView foodCalories;
+    TextView foodPrice;
+    ImageView vegRating;
+    ImageView imageView;
+    int position;
+}
